a43e9eef7367a19a4068083e6dc6d1f0552952ab
Jack Hughes <jack@jackhughesweb.com>
2015-10-24 22:43:25 +0100

    EVERYTHING

--- question.php
+++ question.php
@@ -1,16 +1,16 @@
 <?php include('templates/header.php');
 
 $round = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'round'"));
-$round = $round[2];
+$round = substr($round[2], 0, 1);
 $img = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM images WHERE id = $round"));
 $url = $img[0];
-$tags_and_probs = explode(",", $img[1]);
-$tags = [];
-foreach($tags_and_probs as $key => $val){
-	$exploded = explode(";", $val);
-	$tags[$key] = $exploded[0];
-	$probs[$key] = $exploded[1];
-}
+// $tags_and_probs = explode(",", $img[1]);
+// $tags = [];
+// foreach($tags_and_probs as $key => $val){
+// 	$exploded = explode(";", $val);
+// 	$tags[$key] = $exploded[0];
+// 	$probs[$key] = $exploded[1];
+// }
 ?>
 <body>
 	<div class="desktop">
--- start.php
+++ start.php
@@ -19,15 +19,43 @@ if ($game_state == "0" && !isset($_GET['state'])) { ?>
 if ($game_state == "1" || isset($_GET['state'])) {
 	push_new_q($pusher);
 	$blah = mysqli_query($link, "UPDATE settings SET val='1' WHERE field = 'state'");
+	$round = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'round'"));
+	$round = substr($round[2], 0, 1);
+	$img = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM images WHERE id = $round"));
+	$url = $img[0];
 ?>
 <body class="host" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;"> 
 	<div class="desktop centre">
 		<header class="logo">Pointif<span class="text-blue">ai</span></header>
-		<h2>Waiting for playas<span class="infinite flash">...</span></h2>
-		<h3>Current players:</h3>
-		<ul class="player-list">
-		</ul>
-		<a href="?state=1" class="button button-blue button-cta">Start Game</a>
+		<table class="question-view">
+			<tr>
+			<td><img src="<?php echo $url ?>" alt="Question Image" class="question-image"></td>
+				<td>
+					<table class="leaderboard">
+						<tr>
+							<td>100</td>
+							<td>clarifai-bob</td>
+							<td>...</td>
+						</tr>
+						<tr>
+							<td>75</td>
+							<td>clarifai-dave</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>50</td>
+							<td>clarifai-sarah</td>
+							<td>...</td>
+						</tr>
+						<tr>
+							<td>25</td>
+							<td>clarifai-user</td>
+							<td>...</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
 	</div>
 </body>
 <?php
