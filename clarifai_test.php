f026c967fbfb46e4b09f7a4cfe18ba5cce3d8a9e
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-24 15:45:27 +0100

    Added removal of minimum value from tags array

--- clarifai_test.php
+++ clarifai_test.php
@@ -19,6 +19,31 @@ else {
 	$img_results = $json["results"]["0"]["result"]["tag"];
 	$tags = $img_results["classes"];
 	$probs = $img_results["probs"];
+	print_tags($tags, $probs);
+	$modified = remove_min($tags, $probs);
+	$tags = $modified[0];
+	$probs = $modified[1];
+	$min_val = $modified[2];
+	print_tags($tags, $probs);
+}
+
+function remove_min($tags, $probs){
+	$min_val = INF;
+	$min_index = "0";
+	foreach($tags as $key => $val){
+		$probs[$key] = floatval($probs[$key]);
+		if($probs[$key] < $min_val) {
+			$min_val = $probs[$key];
+			$min_index = $key;
+		}
+	}
+	unset($tags[$min_index]);
+	unset($probs[$min_index]);
+	return array ($tags, $probs, $min_val);
+}
+
+function print_tags($tags, $probs){
+	echo "<br>Count: " . count($tags) . "<br>";
 	foreach($tags as $key => $val){
 		echo $val . " = " . $probs[$key] . "<br>";
 	}
