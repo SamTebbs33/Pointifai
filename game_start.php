3c7f6c065182c18977b0e0ed03fa7e00a7cecf66
Jack Hughes <jack@jackhughesweb.com>
2015-10-24 17:12:31 +0100

    Updated file

--- game_start.php
+++ game_start.php
@@ -2,8 +2,8 @@
 
 include "config.php";
 
-$round = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM misc WHERE field = 'round'"));
-$num_pps = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM misc WHERE field = 'num_pps'"));
+$round = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'round'"));
+$num_pps = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'num_pps'"));
 echo "Round: " . $round . "<br>";
 echo "Num pps: " . $num_pps . "<br>";
 
