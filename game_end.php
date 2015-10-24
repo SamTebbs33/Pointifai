b067fdb8972de65b21a7e99e08fe38bb6d7e1003
aranscope <aranlong@live.co.uk>
2015-10-24 19:21:11 +0100

    Added more animations to end screen

--- game_end.php
+++ game_end.php
@@ -6,7 +6,7 @@
 	</script>
 	<div class="welcome">
 		<h1 class="logo animated infinite pulse">Samuel Tebbs</h1>
-		<h2>100 pts</h2>
+		<h2 class="animated infinite shake">100 pts</h2>
 	</div>
 </body>
 <?php include('templates/footer.php'); ?>
