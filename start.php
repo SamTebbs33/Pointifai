3116f80d58d1d223ae33f6540b89af129dd8b0da
aranscope <aranlong@live.co.uk>
2015-10-24 20:26:46 +0100

    Styling start.php

--- start.php
+++ start.php
@@ -4,7 +4,7 @@
 		<header class="logo">Pointif<span class="text-blue">ai</span></header>
 		<h2>Waiting for players<span class="infinite flash">...</span></h2>
 		<h3>Current players:</h3>
-		<ul>
+		<ul class="player-list">
 			<li>Player 1</li>
 			<li>Player 2</li>
 			<li>Player 3</li>
