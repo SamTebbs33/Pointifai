d3fb3bc06aa8702ce1a5e02b7256164b31937439
Jack Hughes <jack@jackhughesweb.com>
2015-10-25 00:17:05 +0100

    i give up

--- start.php
+++ start.php
@@ -3,7 +3,7 @@
 $game_state = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'state'"))[2];
 
 if ($game_state == "0" && !isset($_GET['state'])) { ?>
-<body class="host" data-state="<?php echo $game_state; ?>" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;"> 
+<body class="host" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;"> 
 	<div class="desktop centre">
 		<header class="logo">Pointif<span class="text-blue">ai</span></header>
 		<h2>Waiting for players<span class="infinite flash">...</span></h2>
@@ -24,7 +24,7 @@ if ($game_state == "1" || isset($_GET['state'])) {
 	$img = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM images WHERE id = $round"));
 	$url = $img[1];
 ?>
-<body class="host<?php if(strlen($game_state) == 1) { echo " timer"; } ?>" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;"> 
+<body class="host<?php if(strlen($game_state) == 1) { echo " timer"; } ?>" data-state="<?php echo $game_state; ?>" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;"> 
 	<div class="desktop centre">
 		<header class="logo">Pointif<span class="text-blue">ai</span></header>
 		<?php if(strlen($game_state) == 1) { ?>
