4f1261f57891dc787b48d2e184fa2dcbfa3b2445
Jack Hughes <jack@jackhughesweb.com>
2015-10-24 23:10:11 +0100

    Fixed default score and rank

--- join.php
+++ join.php
@@ -15,8 +15,8 @@ if (mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = '
 	</div>
 	<div class="mobile mobile-enter-tag">
 		<header class="logo">Pointif<span class="text-blue">ai</span></header>
-		<p class="score">100</p>
-		<p class="rank">2nd</p>
+		<p class="score">0</p>
+		<p class="rank">1st</p>
 		<p><input type="text" placeholder="Enter tag..." style="width: 100%;"></p>
 	</div>
 	<div class="mobile mobile-countdown">
