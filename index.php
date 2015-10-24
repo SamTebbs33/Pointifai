e8a9be6e223d547df440b455f88756fbe733b389
Jack Hughes <jack@jackhughesweb.com>
2015-10-24 13:49:35 +0100

    Added basic welcome page

--- index.php
+++ index.php
@@ -3,8 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Pointifai</title>
+	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-	
+	<div class="welcome">
+		<h1 class="logo">Pointif<span class="text-blue">ai</span></h1>
+		<h2>A pointless game for pointless people</h2>
+		<a href="/start" class="button">Start a new game</a>
+		<br>
+		<a href="/join" class="button">Join a game</a>
+	</div>
 </body>
 </html>
