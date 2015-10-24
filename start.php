8766a284ea0653b74ec225369722af22f6e3150b
aranscope <aranlong@live.co.uk>
2015-10-24 19:51:19 +0100

    Styling start.php

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 <body>
 	<div class="desktop centre">
 		<header class="logo">Pointif<span class="text-blue">ai</span></header>
-		<h2>Waiting for players...</h2>
+		<h2>Waiting for players<span class="infinite flash">...</span></h2>
 		<h3>Current players:</h3>
 		<ul>
 			<li>Player 1</li>
@@ -10,7 +10,7 @@
 			<li>Player 3</li>
 			<li>Player 4</li>
 		</ul>
-		<a class="button">Start Game</a>
+		<a href="" class="button button-blue button-cta">Start Game</a>
 	</div>
 </body>
 <?php include('templates/footer.php'); ?>
