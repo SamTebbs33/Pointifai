f89b00b73b20e7ebd1455c325cc49092099057c6
aranscope <aranlong@live.co.uk>
2015-10-24 20:37:29 +0100

    Styling start.php

--- start.php
+++ start.php
@@ -1,6 +1,6 @@
 <?php include('templates/header.php'); ?>
 <body class="host" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;">
-	<div class="desktop">
+	<div class="desktop centre">
 		<header class="logo">Pointif<span class="text-blue">ai</span></header>
 		<h2>Waiting for players<span class="infinite flash">...</span></h2>
 		<h3>Current players:</h3>
