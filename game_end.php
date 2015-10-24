bef0c3ab0e5848774a67ad23bd6071980f2bf206
Aran Long <aranlong@live.co.uk>
2015-10-24 18:02:45 +0100

    Update game_end.php

--- game_end.php
+++ game_end.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Pointifai</title>
-	<link rel="stylesheet" href="/css/style.css">
-</head>
+<?php include('templates/header.php'); ?>
 <body style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;">
-	<div class="endscreen">
-		<h2 class="winner-name">Name</h1>
-		<h1 class = "winner-points">Points</h1>
+	<div class="welcome">
+		<h1 class="logo">Samuel Tebbs</h1>
+		<h2>100 pts</h2>
 	</div>
 </body>
-</html>
+<?php include('templates/footer.php'); ?>
