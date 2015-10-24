faf6f75c9bc078765a8b94a143599889f10ae0ef
Aran Long <aranlong@live.co.uk>
2015-10-24 17:53:38 +0100

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -5,6 +5,6 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Pointifai</title>
-	<<link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway:700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="/css/style.css">
 </head>
