fb75946a9c353da64f4bbf8b35cc3c59893d8b01
Jack Hughes <jack@jackhughesweb.com>
2015-10-24 13:56:35 +0100

    Added desktop view start.php

---
+++ start.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pointifai</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+	<div class="desktop">
+		<header class="logo">Pointif<span class="text-blue">ai</span></header>
+		<h2>Waiting for players...</h2>
+		<h3>Current players:</h3>
+		<ul>
+			<li>Player 1</li>
+			<li>Player 2</li>
+			<li>Player 3</li>
+			<li>Player 4</li>
+		</ul>
+		<a class="button">Start Game</a>
+	</div>
+</body>
+</html>
