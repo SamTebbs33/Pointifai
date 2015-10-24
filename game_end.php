9f7f63c4b2ba52635edc38e4295a030636ce0f54
AranScope <aranlong@live.co.uk>
2015-10-24 16:27:14 +0100

    Added background image

--- game_end.php
+++ game_end.php
@@ -5,7 +5,7 @@
 	<title>Pointifai</title>
 	<link rel="stylesheet" href="/css/style.css">
 </head>
-<body>
+<body style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;">
 	<div class="endscreen">
 		<h2 class="winner-name">Name</h1>
 		<h1 class = "winner-points">Points</h1>
