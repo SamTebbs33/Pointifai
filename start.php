0a386b2da8b4a964a6246a1b3e6015888c3f62a1
Jack Hughes <jack@jackhughesweb.com>
2015-10-24 22:08:13 +0100

    Adding game state 1

--- start.php
+++ start.php
@@ -1,12 +1,34 @@
-<?php include('templates/header.php'); ?>
-<body class="host" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;">
+<?php include('templates/header.php'); 
+
+$game_state = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'state'"))[2];
+
+if ($game_state == "0" && !isset($_GET['state'])) { ?>
+<body class="host" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;"> 
 	<div class="desktop centre">
 		<header class="logo">Pointif<span class="text-blue">ai</span></header>
 		<h2>Waiting for players<span class="infinite flash">...</span></h2>
 		<h3>Current players:</h3>
 		<ul class="player-list">
 		</ul>
-		<a href="" class="button button-blue button-cta">Start Game</a>
+		<a href="?state=1" class="button button-blue button-cta">Start Game</a>
 	</div>
 </body>
-<?php include('templates/footer.php'); ?>
+<?php 
+}
+
+if ($game_state == "1" || isset($_GET['state'])) {
+
+?>
+<body class="host" style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;"> 
+	<div class="desktop centre">
+		<header class="logo">Pointif<span class="text-blue">ai</span></header>
+		<h2>Waiting for players<span class="infinite flash">...</span></h2>
+		<h3>Current players:</h3>
+		<ul class="player-list">
+		</ul>
+		<a href="?state=1" class="button button-blue button-cta">Start Game</a>
+	</div>
+</body>
+<?php
+}
+include('templates/footer.php'); ?>
