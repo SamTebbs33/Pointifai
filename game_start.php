8afe3b23a48093c45a4614a9e76f4617c7fc685d
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-24 18:22:53 +0100

    more stuff

--- game_start.php
+++ game_start.php
@@ -7,7 +7,9 @@ $round = $round[2];
 $num_pps = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'num_pps'"));
 $num_pps = $num_pps[2];
 
+echo "checking round<br>";
 if($round <= 5){
+	echo "checked round<br>";
 	$round = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM images WHERE id = '$round'"));
 	$url = $round[1];
 	$tags = $round[2];
