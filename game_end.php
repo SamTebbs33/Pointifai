1da99a0bbbbd19f20fde79ce8136995f7ba7343f
AranScope <aranlong@live.co.uk>
2015-10-24 15:38:59 +0100

    game-end.php added, endscreen css style added

---
+++ game_end.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pointifai</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+	<div class="endscreen">
+		<h2 class="winner-name">Name</h1>
+		<h1 class = "winner-points">Points</h1>
+	</div>
+</body>
+</html>
