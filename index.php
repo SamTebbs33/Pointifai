9bed5b0bf89e1d015e5524c67b51121e6e1e0f20
Jack Hughes <jack@jackhughesweb.com>
2015-10-24 14:59:02 +0100

    Fixed links on homepage

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 	<title>Pointifai</title>
 	<link rel="stylesheet" href="/css/style.css">
 </head>
-<body style="background-image: url('/img/stars.jpeg'); background-size:cover; color: white;">
+<body style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;">
 	<div class="welcome">
 		<h1 class="logo">Pointif<span class="text-blue">ai</span></h1>
 		<h2>A pointless game for pointless people</h2>
-		<a href="/start" class="button button-white-stroke">Start a new game</a>
-		<a href="/join" class="button button-blue">Join a game</a>
+		<a href="/start.php" class="button button-white-stroke">Start a new game</a>
+		<a href="/join.php" class="button button-blue">Join a game</a>
 	</div>
 </body>
 </html>
