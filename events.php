cd4a185c78dff4212224f19c14d848c04caca6b8
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-24 21:17:23 +0100

    Added pusher parameter to functions in events.php

--- events.php
+++ events.php
@@ -5,19 +5,19 @@ require_once "config.php";
 $pusher = new Pusher($key_pusher_id, $key_pusher_secret, $key_pusher_app_id);
 
 // Client
-function push_new_q(){
+function push_new_q($pusher){
 	$pusher->trigger("client-channel", "new_q", "");
 }
 
-function push_score_update(){
+function push_score_update($pusher){
 	$pusher->trigger("client-channel", "score_update", get_leaderboard());	
 }
 
-function push_game_end(){
+function push_game_end($pusher){
 	$pusher->trigger("client-channel", "game_end", "");
 }
 
 // Host
-function push_new_registration($user){
+function push_new_registration($pusher, $user){
 	$pusher->trigger("host-channel", "new-registration", $user);
 }
