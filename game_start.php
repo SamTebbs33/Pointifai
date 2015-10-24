38a0d6b7f14a0db05650e35c9dce78ebc868fa77
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-24 18:35:20 +0100

    changed from mysqli to other method

--- game_start.php
+++ game_start.php
@@ -11,8 +11,11 @@ if($round <= 5){
 	$round = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM images WHERE id = '$round'"));
 	$url = $round[1];
 	$tags = $round[2];
-	$pps = mysqli_fetch_all(mysqli_query($link, "SELECT * FROM participants"));
-	var_dump($pps);
+	$pps = mysqli_query($link, "SELECT * FROM participants");
+	while($row = $pps->fetch_assoc()){
+		var_dump($row);
+		echo "<br>";
+	}
 }
 
 ?>
