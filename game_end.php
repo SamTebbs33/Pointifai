62d92d88b20d48362dd0ae12549ce64d10b5230e
aranscope <aranlong@live.co.uk>
2015-10-24 18:36:28 +0100

    Added outro theme

--- game_end.php
+++ game_end.php
@@ -1,5 +1,9 @@
 <?php include('templates/header.php'); ?>
 <body style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;">
+	<script>
+		var audio = new Audio('audio/end_theme.mp3');
+		audio.play();
+	</script>
 	<div class="welcome">
 		<h1 class="logo">Samuel Tebbs</h1>
 		<h2>100 pts</h2>
