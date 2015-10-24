0d6cea87ca8cd63fa7d01d237c75b59e27f1cbd2
Jack Hughes <jack@jackhughesweb.com>
2015-10-24 21:31:02 +0100

    Oh no!

--- register.php
+++ register.php
@@ -4,4 +4,4 @@ require_once 'config.php';
 
 $round = mysqli_query($link, "INSERT INTO `participants`(`name`, `points`) VALUES ('" . mysqli_escape_string($link, $_POST['name']) . "', 0)");
 
-push_new_registration(htmlspecialchars($_POST['name']));
+push_new_registration($pusher, htmlspecialchars($_POST['name']));
