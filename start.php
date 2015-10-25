024accc0b4fb550a86eeddbbce79c7e0164cc2c2
Jack Hughes <jack@jackhughesweb.com>
2015-10-25 01:26:42 +0100

    awesomised winner page #winning

--- start.php
+++ start.php
@@ -78,7 +78,17 @@ else if ($game_state > 0 && ($game_state % 2 == 0) && $game_state < 9) {
 }
 else if ($game_state > 8) {
 	?>
-WINNER!!!!!!
+<body style="background-image: url('/img/stars.jpeg'); background-size:cover; background-position: center; color: white;">
+	<script>
+		var audio = new Audio('audio/end_theme.wav');
+		audio.play();
+	</script>
+	<div class="welcome">
+		<h1 class="logo animated infinite pulse winner_name">Samuel Tebbs</h1>
+		<h2 class="winner_points">100 pts</h2>
+		<a href="index.php" class="button button-white-stroke button-cta">Back to menu</a>
+	</div>
+</body>
 	<?php
 }
 include('templates/footer.php'); ?>
