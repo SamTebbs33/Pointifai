8d6c0a83d470d95da9d734a31584c6d90c88ef37
Jack Hughes <jack@jackhughesweb.com>
2015-10-25 11:27:51 +0000

    undoing #yolo 😞

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	<div class="welcome">
 		<h1 class="logo">Pointif<span class="text-blue">ai</span></h1>
 		<h2>A pointless game for pointless people</h2>
-		<a href="/start.php" class="button button-white-stroke button-cta"><span class="hover-hide">Start a new game</span><span class="hover-show">#yolo</span></a>
-		<a href="/join.php" class="button button-blue button-cta"><span class="hover-hide">Join a game</span><span class="hover-show">#yolo</span></a>
+		<a href="/start.php" class="button button-white-stroke button-cta">Start a new game</a>
+		<a href="/join.php" class="button button-blue button-cta">Join a game</a>
 	</div>
 </body>
 <?php include('templates/footer.php'); ?>
