c5c7c3b50060045871355f88d1427de760a7373e
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-25 08:15:36 +0000

    This may work

--- clarifai_test.php
+++ clarifai_test.php
@@ -79,7 +79,7 @@ function add_img_to_db($img_url){
 	var_dump($res);
 	if($res == false){
 		echo "Doesn't exist<br>";
-		mysqli_query($link, "INSERT INTO images (url, tags probs) VALUES ('" . $tags_and_probs[0] . "', " . $tags_and_probs[1] . ")");
+		$z = mysqli_query($link, "INSERT INTO images (url, tags probs) VALUES ('" . $tags_and_probs[0] . "', " . $tags_and_probs[1] . ")");
 		echo "Should have inserted<br>";
 	}else{
 		echo "exists<br>";
