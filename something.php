26afc05b590be9e601ffced626f821aabf956174
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-25 09:40:55 +0000

    Trying to fix something.php

--- something.php
+++ something.php
@@ -8,15 +8,19 @@ $tag = $_POST["tag"];
 
 $game_state = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'state'"))[2];
 $tags_and_probs = explode(",", mysqli_fetch_row(mysqli_query($link, "SELECT * FROM images WHERE id = " . mysqli_real_escape_string($link, $q_id))));
-$tags = explode(",", $tags_and_probs[0]);
-$probs = explode(",", $tags_and_probs[1]);
+var_dump($tags_and_probs);
+$tags = explode(",", $tags_and_probs["tags"]);
+$probs = explode(",", $tags_and_probs["probs"]);
+var_dump($tags);
+var_dump($probs);
 if($q_id == ($game_state + 1) / 2){
+	echo "q id ok<br>";
 	foreach($tags as $key => $val){
 		if($tags[$key] == $tag){
 			$name = mysqli_real_escape_string($link, $name);
 			$pts = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM participants WHERE name = $name"));
 			$pts += 100 - intval($probs[$key]);
-			mysql_query($link, "UPDATE participants SET points = " . intval($pts) . " WHERE name = $name");
+			mysql_query($link, "UPDATE participants SET points = '" . intval($pts) . "' WHERE name = $name");
 		}
 	}
 }
