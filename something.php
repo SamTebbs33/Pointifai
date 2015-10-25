69da02b03600da99203f341a82e1765031a38175
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-10-25 10:03:20 +0000

    yo

--- something.php
+++ something.php
@@ -6,13 +6,11 @@ $name = $_POST["name"];
 $q_id = $_POST["q_id"];
 $tag = $_POST["tag"];
 
-$game_state = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'state'"))["val"];
+$game_state = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM settings WHERE field = 'state'"));
+$game_state = $game_state["val"];
 $tags_and_probs = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM images WHERE id = " . mysqli_real_escape_string($link, $q_id)));
-var_dump($tags_and_probs);
 $tags = explode(",", $tags_and_probs["tags"]);
 $probs = explode(",", $tags_and_probs["probs"]);
-var_dump($tags);
-var_dump($probs);
 echo "Q ID: $g_id<br>";
 echo "State: $game_state<br>";
 if(intval($q_id) == (intval($game_state) + 1) / 2){
@@ -23,6 +21,7 @@ if(intval($q_id) == (intval($game_state) + 1) / 2){
 			$pts = mysqli_fetch_row(mysqli_query($link, "SELECT * FROM participants WHERE name = $name"));
 			$pts += 100 - intval($probs[$key]);
 			mysql_query($link, "UPDATE participants SET points = '" . intval($pts) . "' WHERE name = $name");
+			break;
 		}
 	}
 }
